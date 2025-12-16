900e022c760ca34eff1737393c7cf49d43b86858
redkyo017 <redkyo017@gmail.com>
2025-12-16 16:42:24 +0700

    week 3 day 20

--- main.go
+++ main.go
@@ -7,17 +7,15 @@ import (
 
 func main() {
 	log.Println("con co be be")
-	// nums := []int{-1, 4, 2, 1, 9, 10}
-	// nums := []int{-1, -2, -60, 40, 43}
 	node1 := week3.ListNode{1, nil}
 	node2 := week3.ListNode{2, nil}
-	// node3 := week3.ListNode{3, nil}
-	// node4 := week3.ListNode{4, nil}
-	// node5 := week3.ListNode{5, nil}
+	node3 := week3.ListNode{3, nil}
+	node4 := week3.ListNode{4, nil}
+	node5 := week3.ListNode{5, nil}
 	node1.Next = &node2
-	// node2.Next = &node3
-	// node3.Next = &node4
-	// node4.Next = &node5
-	res := week3.RemoveNthFromEnd(&node1, 2)
+	node2.Next = &node3
+	node3.Next = &node4
+	node4.Next = &node5
+	res := week3.RotateRight(&node1, 2)
 	log.Println("res", res)
 }
