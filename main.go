07972c18326dd9ce0ed5b64a029f5c115cc85ced
redkyo017 <redkyo017@gmail.com>
2025-12-30 15:28:45 +0700

    finalize week 4 && move to week 5

--- main.go
+++ main.go
@@ -17,6 +17,6 @@ func main() {
 	// node3.Next = &node4
 	// node4.Next = &node5
 	// res := week4.BackspaceCompare("xywrrmp", "xywrrmu#p")
-	res := week4.DecodeString("3[a2[c]]")
+	res := week4.LargestRectangleArea([]int{2, 1, 5, 6, 2, 3})
 	log.Println("con co", res)
 }
